b1d9cad334c205f431cbba7fd9dbf514a5129529
Anne Esther Lins Figueirôa <anne.figueiroa@sou.inteli.edu.br>
2025-03-07 15:11:10 -0300

    assets

--- game.js
+++ game.js
@@ -1,8 +1,12 @@
 var config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#00000000',
-    width: 800,
-    height: 600,
+    type: Phaser.AUTO, // tipo de renderização
+    backgroundColor: '#00000000', // cor do fundo
+    width: 1000, // largura da tela 
+    height: 600, // altura da tela
+    scale: {
+        mode: Phaser.Scale.FIT // ajustar para caber na tela sem distorcer
+    },
+    autoCenter: Phaser.Scale.CENTER, // centralizar a tela do jogo
     scene: {
         preload: preload,
         create: create
@@ -18,6 +22,6 @@ function preload() {
 
 function create() {
     // Adicionar texto usando a fonte carregada
-    this.add.text(100, 100, 'Não', { fontFamily: 'DefaultFont', fontSize: '50px', fill: '#ffffff' });
+    this.add.text(100, 100, 'Utilize WASD para se movimentar e Space para pular.', { fontFamily: 'DefaultFont', fontSize: '30px', fill: '#ffffff', wordWrap: {width: 800}});
     this.background.color
 }
