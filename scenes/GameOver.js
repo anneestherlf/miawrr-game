6515502d8d16768af145938f24c92489296ac034
Anne Esther Lins Figueirôa <anne.figueiroa@sou.inteli.edu.br>
2025-03-07 22:06:51 -0300

    game_02

--- scenes/GameOver.js
+++ scenes/GameOver.js
@@ -2,4 +2,24 @@ class GameOver extends Phaser.Scene {
     constructor() {
         super({ key: 'GameOver' });
     }
+
+    preload () {
+        this.load.image('PanelGameOver', './assets/panels/PanelGameOver.png');
+    }
+
+    create() {
+        const PanelGameOver = this.add.image(larguraJogo/2, alturaJogo/2, 'PanelGameOver').setInteractive(); // Adiciona a imagem ao centro 
+        PanelGameOver.setDisplaySize(462.5, 462.5);
+
+        PanelGameOver.on('pointerdown', () => {
+            this.scene.start('Jogo');
+        });
+
+    }
+
+    update () {
+
+    }
+
+    
 }
--- scenes/Parabens.js
+++ scenes/Parabens.js
@@ -2,4 +2,22 @@ class Parabens extends Phaser.Scene {
     constructor() {
         super({ key: 'Parabens' });
     }
+
+    preload () {
+        this.load.image('PanelParabens', './assets/panels/PanelParabens.png');
+    }
+
+    create() {
+        const PanelParabens = this.add.image(larguraJogo/2, alturaJogo/2, 'PanelParabens').setInteractive(); // Adiciona a imagem ao centro 
+        PanelParabens.setDisplaySize(462.5, 462.5);
+
+        PanelParabens.on('pointerdown', () => {
+            this.scene.start('TelaInicial');
+        });
+
+    }
+
+    update () {
+
+    }
 }
